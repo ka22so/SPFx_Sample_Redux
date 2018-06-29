3e88755c7f388c11d48bde4eaebcfd5f0bacd8f0
naoki_katsumi <naoki_katsumi@2fc-group.jp>
2018-06-29 10:03:21 +0900

    Add: store

---
+++ SPFx_Sample_Redux/src/webparts/spFxSampleRedux/store/SPFxStore.ts
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, Store } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import SPFxReducer from '../reducers/SPFxReducer';
+import { ListState } from '../state/ListState';
+
+const loggerMiddleware = createLogger();
+
+export default function configureStore() {
+
+  //do not use loggerMiddleware in production
+  const listSateStore: Store<ListState> = createStore<ListState>(SPFxReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+
+  return listSateStore;
+}
